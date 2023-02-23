aaf8935a9c8b67533adb695c4242af4bbf3de15d
hamidreza1983 <126154017+hamidreza1983@users.noreply.github.com>
2023-02-23 17:50:44 +0330

    port_scanner_with_python
    sample of security with python code
    use it
    develope it

---
+++ port_scanner.py
@@ -0,0 +1,22 @@
+import socket
+import threading 
+import time
+max_tr=50
+
+#insert your ip here
+ip='127.0.0.1'
+#scan open tcp port
+def port_checking(ipv4:str ,port:int):
+    with open('check.txt','a') as file:
+        socket =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.settimeout(0.5)
+        response =socket.connect_ex((ipv4,port))
+        if  response == 0:
+            file.write(ip+':'+str(port)+'\n')
+            file.close()
+    socket.close()
+#use threading for highspeed and high performance
+for port in range(1,10000):
+    threading.Thread(target=port_checking,args=(ip,port)).start()
+    while threading.active_count() >max_tr:
+        time.sleep(0.1)
